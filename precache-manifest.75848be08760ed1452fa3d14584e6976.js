f5108f5c65c4dba6fc38bd7ad582561166877d2d
tangyou <maoyou5749@163.com>
2021-02-17 01:21:25 +0800

    deploy

---
+++ precache-manifest.75848be08760ed1452fa3d14584e6976.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e07636009b5055f62137",
+    "url": "/Sweetsugar-website/css/app.12dbaeeb.css"
+  },
+  {
+    "revision": "3c676fc91d102d0880ed739b35619ec8",
+    "url": "/Sweetsugar-website/index.html"
+  },
+  {
+    "revision": "e07636009b5055f62137",
+    "url": "/Sweetsugar-website/js/app.e147cb13.js"
+  },
+  {
+    "revision": "3b8bd348255987941fbc",
+    "url": "/Sweetsugar-website/js/chunk-vendors.35842e9c.js"
+  },
+  {
+    "revision": "87e781ca27fd6ccc56283519ba4f1e6d",
+    "url": "/Sweetsugar-website/manifest.json"
+  },
+  {
+    "revision": "020bd38cc2985cc628db5ebc54f764df",
+    "url": "/Sweetsugar-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Sweetsugar-website/robots.txt"
+  }
+]);
